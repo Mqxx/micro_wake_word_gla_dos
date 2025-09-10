b414abcaf131b64486ce918cc7a0e17eda5dfb66
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-09-10 22:29:59 +0200

    Update compute_arena.js

--- compute_arena.js
+++ compute_arena.js
@@ -1,20 +1,11 @@
 const FindArena = require('tflite-find-arena-size');
-const fs = require('fs').promises;
-
+const fs = require('fs');
+ 
 (async () => {
-  const fa = new FindArena();
-  await fa.init();
-
-  const models = process.argv.slice(2);
-  if (models.length === 0) {
-    console.error("Keine Modelle übergeben.");
-    process.exit(1);
-  }
-
-  for (const m of models) {
-    const buf = await fs.readFile(m);
-    const size = await fa.findArenaSize(buf, 1024, 128 * 1024); // 1 KB .. 128 KB
-    console.log(`\n📦 Modell: ${m}`);
-    console.log(`   Empfohlene tensor_arena_size = ${size} Bytes`);
-  }
+    let fa = new FindArena();
+    await fa.init();
+ 
+    let tflite = await fs.promises.readFile('models/Gla_DOS.tflite');
+    let size = await fa.findArenaSize(tflite, 1 * 1024, 128 * 1024);
+    console.log('arena size is', size);
 })();
